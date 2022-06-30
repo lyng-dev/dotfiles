917a3e7efc4971da33f26c3ddcaf593edbbd1e28
Steffen Mads Lyng <73419918+lyng-dev@users.noreply.github.com>
2022-06-30 20:15:28 +0200

    added myip function

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
 # make directory and nagivate to it
-mkcd() { mkdir -p "$1" && cd "$1"; } 
+mkcd() { mkdir -p "$1" && cd "$1"; }
+
+# show my ip, and a bit more info
+myip() { curl -qs https://ifconfig.co/json | jq -r '.ip,.city,.country,.hostname,.asn_org' }
